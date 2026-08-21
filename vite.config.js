df6cd31ec44a0787955a19c574398f01e991e97c
lucifer046 <prakashdivya552@gmail.com>
2026-08-21 17:48:51 +0530

    chore: enable seamless root base path when deploying to Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/comrade_website/',
+  base: process.env.VERCEL ? '/' : '/comrade_website/',
   plugins: [react()],
   server: {
     port: 5173,
